3968d0fd2c3f911e873542ee935958c675b0aa23
fiecueal <fiecueal@proton.me>
2025-03-22 20:17:04 -0700

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # creates new profile if $1 does not exist
-if [ "$1" ]; then
-  profile=$(find $HOME/.mozilla/firefox/ -maxdepth 1 -name "*.$1")
-else
+profile=$(find $HOME/.mozilla/firefox/ -maxdepth 1 -name "*.$1")
+
+if [ ! "$profile" ]; then
   firefox -CreateProfile "$1"
   sleep 2 # give firefox time to create the new profile
-  profile=$(find $HOME/.mozilla/firefox/ -maxdepth 1 -name "*.$1")
 fi
+
+profile=$(find $HOME/.mozilla/firefox/ -maxdepth 1 -name "*.$1")
 
 cp -r --backup=numbered chrome/ user.js $profile
 cat user.js-extras >> $profile/user.js
